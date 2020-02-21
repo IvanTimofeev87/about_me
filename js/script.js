044b88791c6f0a1b50465bc0707f01f843df9a3d
Timofeev <ivant87@mail.ru>
2020-02-21 16:16:33 +0300

    создал русскую и английскую версию main

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/*Скрипты для космонавта */
+
 var spaceman = document.querySelector(".spaceman-div");
 
 spaceman.addEventListener("click", function(evt) {
@@ -7,4 +9,27 @@ spaceman.addEventListener("click", function(evt) {
   } else {
     spaceman.classList.add("spaceman-shake");
   }
+});
+
+/*скрипты для кнопок*/
+
+var ru_button = document.querySelector(".rus_button");
+var en_button = document.querySelector(".eng_button");
+var ru_main = document.querySelector(".rus-main");
+var en_main = document.querySelector(".eng-main");
+
+ru_button.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  if (ru_main.classList.contains("visually-hidden")) {
+    ru_main.classList.remove("visually-hidden");
+    en_main.classList.add("visually-hidden");
+  };
+});
+
+en_button.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  if (en_main.classList.contains("visually-hidden")) {
+    en_main.classList.remove("visually-hidden");
+    ru_main.classList.add("visually-hidden");
+  };
 });
