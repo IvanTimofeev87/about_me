df575aad2a179b5396e22a3183cb71eb378b4d70
Timofeev Ivan <ivant87@mail.ru>
2020-02-23 14:44:39 +0300

    добавил анимацию исчезновения для космонавта

--- js/script.js
+++ js/script.js
@@ -2,13 +2,19 @@
 
 var spaceman = document.querySelector(".spaceman-div");
 
-spaceman.addEventListener("click", function(evt) {
+spaceman.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (spaceman.classList.contains("spaceman-shake")) {
     spaceman.classList.remove("spaceman-shake");
-  } else {
+    spaceman.classList.add("spaceman-disappear");
+  } else if (spaceman.classList.contains("spaceman-show")) {
+    spaceman.classList.remove("spaceman-show");
     spaceman.classList.add("spaceman-shake");
+  } else if (spaceman.classList.contains("spaceman-disappear")) {
+    spaceman.classList.remove("spaceman-disappear");
+    spaceman.classList.add("spaceman-show");
   }
+
 });
 
 /*скрипты для кнопок*/
@@ -18,7 +24,7 @@ var en_button = document.querySelector(".eng_button");
 var ru_main = document.querySelector(".rus-main");
 var en_main = document.querySelector(".eng-main");
 
-ru_button.addEventListener("click", function(evt) {
+ru_button.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (ru_main.classList.contains("visually-hidden")) {
     ru_main.classList.remove("visually-hidden");
@@ -26,7 +32,7 @@ ru_button.addEventListener("click", function(evt) {
   };
 });
 
-en_button.addEventListener("click", function(evt) {
+en_button.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (en_main.classList.contains("visually-hidden")) {
     en_main.classList.remove("visually-hidden");
